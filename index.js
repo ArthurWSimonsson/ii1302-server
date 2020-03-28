ca6a6a0b964b8665af3023ed23761719a0a55123
Arthur Simonsson <arturo@MacBook-Pro.lan>
2020-03-28 23:04:31 +0100

    manifest

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ var app = express();
 
 const port = 3000
 
+
+var cfenv = require('cfenv');
+var appEnv = cfenv.getAppEnv();
+
 app.get('/get', (req, res) => res.send('Hello World!'))
 
-app.post()
+// app.post()
 
 
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
+app.listen(appEnv.port, '0.0.0.0', function() {
 
+	// print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
+});
